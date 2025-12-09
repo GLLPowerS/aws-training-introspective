0200130a75ebab5ccde77cc99e8c30e78a9caada
Justinas Petrauskas <justinas.petrauskas@cognizant.com>
2025-12-09 11:08:37 +0200

    FIX

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_policy" "sns_sqs" {
   name        = "introspect-sns-sqs-pubsub"
   description = "SNS/SQS access for Dapr pubsub"
-  policy      = file("../infra/iam-policies/sns-sqs-pubsub-policy.json")
+  policy      = file("iam-policies/sns-sqs-pubsub-policy.json")
 
   tags = {
     Project = "introspect"
